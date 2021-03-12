9ca9530c772db56c6720a945f7dc542bf632d34e
XueyanZh <54363064+XueyanZh@users.noreply.github.com>
2021-03-12 04:08:18 -0500

    ASMR-URP

---
+++ Assets/Scripts/StickyWidget.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWidget : MonoBehaviour {
+    public bool attached = false;
+    public GameObject attachedObject;
+    private bool xAxisEnabled = false;
+    private bool zAxisEnabled = false;
+    private bool orbitEnabled = false;
+    public GameObject directionIndicator;
+    [System.Serializable]
+    public enum AutoMovementType
+    {
+        BackAndForth,
+        OrbitRight,
+        OrbitLeft,
+        Stationary,
+        OrbitAroundPlayer
+    }
+    public AutoMovementType movementType;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//        directionIndicator.transform.LookAt(transform.forward);
+        if(attached)
+        {
+        }		
+	}
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Colliding with " + col.gameObject.name);
+        if (!attached)
+        {
+            Debug.Log("not attached yet...");
+            if (col.gameObject.tag == "SoundSphere")
+            {
+                attached = true;
+                Debug.Log("Attaching to " + col.gameObject.name);
+                attachedObject = col.gameObject;
+                attachedObject.transform.parent = gameObject.transform;
+            }
+        }
+    }
+    
+    public void SetMovement()
+    {
+        switch (movementType)
+        {
+            case AutoMovementType.OrbitRight:
+                if (attachedObject.GetComponent<SphereControl>())
+                {
+                    attachedObject.GetComponent<SphereControl>().SetMovement(SphereControl.AutoMovementType.OrbitRight);
+                }
+                break;
+            case AutoMovementType.OrbitLeft:
+                if (attachedObject.GetComponent<SphereControl>())
+                {
+                    attachedObject.GetComponent<SphereControl>().SetMovement(SphereControl.AutoMovementType.OrbitLeft);
+                }
+                break;
+            case AutoMovementType.BackAndForth:
+                if (attachedObject.GetComponent<SphereControl>())
+                {
+                    attachedObject.GetComponent<SphereControl>().SetMovement(SphereControl.AutoMovementType.BackAndForth);
+                }
+                break;
+            case AutoMovementType.Stationary:
+                if (attachedObject.GetComponent<SphereControl>())
+                {
+                    attachedObject.GetComponent<SphereControl>().SetMovement(SphereControl.AutoMovementType.Stationary);
+                }
+                break;
+            case AutoMovementType.OrbitAroundPlayer:
+                if (attachedObject.GetComponent<SphereControl>())
+                {
+                    attachedObject.GetComponent<SphereControl>().SetMovement(SphereControl.AutoMovementType.OrbitAroundPlayer);
+                }
+                break;
+        }
+    }
+
+
+    public void Release()
+    {
+        if(attachedObject)
+        {
+            attachedObject.transform.parent = null;
+            attached = false;
+            attachedObject = null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+    public Transform[] points;
+    private int destinationPoint = 0;
+    private UnityEngine.AI.NavMeshAgent agent;
+    /*    public bool halfPipeMotion;
+    private int currentPoint = 0;
+    private int direction = 1;
+    */
+    // Use this for initialization
+    void Start () {
+
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destinationPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destinationPoint = (destinationPoint + 1) % points.Length;
+    }
+    // Update is called once per frame
+    void Update() {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+            GotoNextPoint();
+        }
+    }
+}
